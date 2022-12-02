dd2765ebf1404245a806a6303098e6e39abb3b59
Ricardo Costa <76919550+R1c4rdCo5t4@users.noreply.github.com>
2022-12-02 12:37:48 +0000

    Update p1.py

--- day1/p1.py
+++ day1/p1.py
@@ -2,13 +2,13 @@
 f = open("puzzle_input.txt", "r")
 puzzle_input = f.read()
 
-max_idx = 0
+elfs = puzzle_input.split("\n\n")
 max_sum = 0
-for idx, elf in enumerate(puzzle_input.split("\n\n"), 1):
+for elf in elfs:
     curr_sum = sum([int(i) for i in elf.split("\n")])
     if curr_sum > max_sum:
         max_sum = curr_sum
-        max_idx = idx
+
 
 print(max_sum) # 72240
 
