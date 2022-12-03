99a49eaffec6635b84aad2c2ab6a16a300b3e286
Ricardo Costa <76919550+R1c4rdCo5t4@users.noreply.github.com>
2022-12-03 14:04:43 +0000

    Day 3 Part 2 Solution Update

--- day3/p2.py
+++ day3/p2.py
@@ -9,9 +9,9 @@ priority_sum = 0
 team_size = 3
 values_dict = { item:value for value,item in enumerate(string.ascii_letters, 1) }
 
-for lineidx in  range(0, len(puzzle_input_lines), team_size):
+for idx in range(0, len(puzzle_input_lines), team_size):
     
-    elf_team = [puzzle_input_lines[lineidx+i] for i in range(team_size)]
+    elf_team = [puzzle_input_lines[idx+i] for i in range(team_size)]
     team_item = functools.reduce(lambda x,y: set(x) & set(y), elf_team)
     priority_sum += values_dict[team_item.pop()]
 
