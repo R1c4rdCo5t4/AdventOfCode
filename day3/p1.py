f031f44a6c67a335b42e6037890e09d86f8ad92d
Ricardo Costa <76919550+R1c4rdCo5t4@users.noreply.github.com>
2022-12-03 19:37:55 +0000

    Day 3 Part 1 Solution Update

--- day3/p1.py
+++ day3/p1.py
@@ -6,14 +6,14 @@ priority_sum = 0
 priority_value = lambda it: ord(it) - (96 if(it.islower()) else 38)
 
 for line in puzzle_input.split("\n"):
-    middle = int(len(line)/2)
+    middle = len(line) // 2
     first = line[:middle]
     second = line[middle:]
 
     for item in second:
         if item in first:
             priority_sum += priority_value(item)
-            break           
+            break
 
 
 print(priority_sum) # 7863
