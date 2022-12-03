e1a0d800bac4b1a69f1242ff128d2536ad4a3f42
Ricardo Costa <76919550+R1c4rdCo5t4@users.noreply.github.com>
2022-12-03 18:19:40 +0000

    Day 3 Part 2 Solution Update

--- day3/p1.py
+++ day3/p1.py
@@ -3,7 +3,7 @@ f = open("puzzle_input.txt", "r")
 puzzle_input = f.read()
 
 priority_sum = 0
-priority_value = lambda it: ord(it) - (96 if(ord(it) in range(ord('a'), ord('z')+1)) else 38)
+priority_value = lambda it: ord(it) - (96 if(it.islower()) else 38)
 
 for line in puzzle_input.split("\n"):
     middle = int(len(line)/2)
