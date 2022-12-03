8c9c48cc7b31092e561fe886d6f71db93178c13d
Ricardo Costa <76919550+R1c4rdCo5t4@users.noreply.github.com>
2022-12-03 13:35:19 +0000

    Day 2 Part 2 Solution Update

--- day2/p2.py
+++ day2/p2.py
@@ -8,27 +8,22 @@
 f = open("puzzle_input.txt", "r")
 puzzle_input = f.read()
 
-#                     losing           drawing          winning
-possible_plays = [['B', 'C', 'A'], ['A', 'B', 'C'], ['C', 'A', 'B']]
+
+# outcome: (outcome points, index-based play score)
+possible_plays = {
+    'X': (0, ['B', 'C', 'A']), #  losing
+    'Y': (3, ['A', 'B', 'C']), #  drawing
+    'Z': (6, ['C', 'A', 'B'])  #  winning
+}
 
 score = 0
 for input_line in puzzle_input.split("\n"):
 
     plays = input_line.split(" ")
     opponent_play = plays[0]
-    game_end = plays[1]
-
-    match(game_end):
-        case "X": # lose
-            score += possible_plays[0].index(opponent_play) + 1
-
-        case "Y": # draw
-            score += possible_plays[1].index(opponent_play) + 4
-            
-        case "Z": # win
-            score += possible_plays[2].index(opponent_play) + 7
+    outcome = plays[1]
+    play = possible_plays[outcome]
+    score += play[0] + play[1].index(opponent_play) + 1
 
 
 print(score) # 11756
-
-    
