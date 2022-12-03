07a43e3428959837d74cee9b11f9243616b7e491
Ricardo Costa <76919550+R1c4rdCo5t4@users.noreply.github.com>
2022-12-03 14:02:36 +0000

    Day 3 Part 2 Update

--- day3/p2.py
+++ day3/p2.py
@@ -1,4 +1,5 @@
 import functools
+import string
 
 f = open("puzzle_input.txt", "r")
 puzzle_input = f.read()
@@ -6,13 +7,12 @@ puzzle_input_lines = puzzle_input.split("\n")
 
 priority_sum = 0
 team_size = 3
-priority_value = lambda it: ord(it) - (96 if(ord(it) in range(ord('a'), ord('z')+1)) else 38)
+values_dict = { item:value for value,item in enumerate(string.ascii_letters, 1) }
 
 for lineidx in  range(0, len(puzzle_input_lines), team_size):
     
     elf_team = [puzzle_input_lines[lineidx+i] for i in range(team_size)]
     team_item = functools.reduce(lambda x,y: set(x) & set(y), elf_team)
-    priority_sum += priority_value(team_item.pop())
+    priority_sum += values_dict[team_item.pop()]
 
 print(priority_sum) # 2488
-
