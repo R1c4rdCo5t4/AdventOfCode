85d2e6a7a0f66a3c563955680e07c9c53b5f925e
Ricardo Costa <76919550+R1c4rdCo5t4@users.noreply.github.com>
2022-12-05 14:12:23 +0000

    Day 5 Part 1 Solution

--- day5/p1.py
+++ day5/p1.py
@@ -24,17 +24,12 @@ f = open("puzzle_input.txt", "r")
 puzzle_input = f.read().split("\n")
 stacks = list(map(lambda x: list(filter(lambda y: y != ' ', x)), get_stacks(puzzle_input))) # gets stacks and filters empty crates
 
-for move_idx in range(len(stacks)+2, len(puzzle_input)): # iterates through move instructions
+for move_idx in range(len(stacks)+1, len(puzzle_input)): # iterates through move instructions
     move = puzzle_input[move_idx].split(" ")
     inst = [int(move[i]) for i in range(1, len(move), 2)] # gets instruction list [amount, from, to]
     move_crate(stacks, inst[0], inst[1]-1, inst[2]-1) # moves crate in stack
-
+    
 
 result = "".join([s[-1] for s in stacks if len(s) > 0]) # joins each element on top of stack in list
 
-print(result) # CMZ
-
-
-### ATTEMPTS ###
-  # LBLVQTVLP
-  # PLVTQVLBL
+print(result) # LBLVVTVLP
